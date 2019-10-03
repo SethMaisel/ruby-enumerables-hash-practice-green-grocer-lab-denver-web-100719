b9247c55963cc1c67bddcc155d00bab515f8e15d
Seth Maisel <seth.maisel@gmail.com>
2019-10-03 01:56:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ def consolidate_cart(cart)
   final_hash = {}
   cart.each do |element_hash|
     element_name = element_hash.keys[0]
-    final_hashelement_hash.values[0]
+    
     
     if 
       final_hash.has_key?(element_name)

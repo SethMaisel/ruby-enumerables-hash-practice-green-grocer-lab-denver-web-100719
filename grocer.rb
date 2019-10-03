5adc2044f47cbc956a8f43704e31dbea3f15eb22
Seth Maisel <seth.maisel@gmail.com>
2019-10-03 02:54:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -26,6 +26,7 @@ def apply_coupons(cart, coupons)
       >= coupon[:num] && !cart["#{item} W/COUPON"]
     cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], 
     count: coupon[:num]}
+    cart[item]
   end
 end
 

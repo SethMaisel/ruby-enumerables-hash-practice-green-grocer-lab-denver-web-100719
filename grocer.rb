390a4b2e291a6c32918bfb8b50a1515f672b8467
Seth Maisel <seth.maisel@gmail.com>
2019-10-03 03:27:48 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,6 +30,7 @@ def apply_coupons(cart, coupons)
     cart["#{item} W/COUPON"][:count] += coupon[:num]
     cart[item][:count] -= coupon[:num]
     end 
+    cart[item][:count] -= coupon[:num]
    end
   end
   cart
